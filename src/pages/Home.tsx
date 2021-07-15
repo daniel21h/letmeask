0a8421b9339e928240244151b694cee9f6497c44
Daniel Hessel <hesseldaniel21@gmail.com>
2021-07-15 11:29:22 -0300

    feat: flow to enter a room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
+import { FormEvent, useState } from "react";
 import { useHistory } from "react-router-dom";
+import { database } from "../services/firebase";
 
 import { Button } from "../components/Button";
 
@@ -14,12 +16,30 @@ export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
 
+  const [roomCode, setRoomCode] = useState('');
+
   async function handleCreateRoom() {
     if (!user) {
       await signInWithGoogle();
     }
 
     history.push("/rooms/new");
+  }
+
+  async function handleJoinRoom(e: FormEvent) {
+    e.preventDefault();
+
+    if (roomCode.trim() === '') return
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists!');
+
+      return
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
@@ -40,10 +60,12 @@ export function Home() {
 
           <div className="separator">ou entre em uma sala</div>
 
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={e => setRoomCode(e.target.value)}
+              value={roomCode}
             />
 
             <Button type="submit">Entrar na sala</Button>
