dcfa8cc774cf93f528b29e2274748aeba09f0764
Daniel Hessel <hesseldaniel21@gmail.com>
2021-07-15 08:35:38 -0300

    feat: environment variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxSP-DDXFreSv5Pdchk6kuBLOJiGG2R_k",
-  authDomain: "letmeask-daniel21h.firebaseapp.com",
-  databaseURL: "https://letmeask-daniel21h-default-rtdb.firebaseio.com",
-  projectId: "letmeask-daniel21h",
-  storageBucket: "letmeask-daniel21h.appspot.com",
-  messagingSenderId: "746916737101",
-  appId: "1:746916737101:web:744f0a002a87f238a6ec04"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
